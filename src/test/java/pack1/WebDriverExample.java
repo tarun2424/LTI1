bde25c6e8ba84031fb8229eb597017982679a68e
tarun2424 <sqatrainertarun@gmail.com>
2021-11-12 15:31:37 +0530

    Changed Browser to Firefox

--- src/test/java/pack1/WebDriverExample.java
+++ src/test/java/pack1/WebDriverExample.java
@@ -1,7 +1,7 @@
 package pack1;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -9,8 +9,11 @@ public class WebDriverExample {
 
 	public static void main(String[] args) {
 
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+	/*	WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();*/
+		
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
 		
 		driver.get("https://blazedemo.com");
 	}
